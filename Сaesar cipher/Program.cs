c76d12247b64e55b39797fc5a4c08ad016946224
ADavlatov <a@davlatov.space>
2022-11-29 21:35:02 +0300

    caesar cipher

--- Сaesar cipher/Program.cs
+++ Сaesar cipher/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<char> cyrillicLetters = new List<char>()
+{
+    'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+    'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+};
+
+int offset = int.Parse(Console.ReadLine()!);
+char[] inputStr = Console.ReadLine()!.ToLower().ToArray();
+
+for (int j = 0; j < inputStr.Length; j++)
+{
+    if (cyrillicLetters.FirstOrDefault(x => x == inputStr[j]) != null)
+    {
+        inputStr[j] = cyrillicLetters[cyrillicLetters.IndexOf(inputStr[j]) + offset];
+    }
+}
+
+Console.WriteLine(string.Join("", inputStr));
